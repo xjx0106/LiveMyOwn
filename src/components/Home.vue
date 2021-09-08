abc8a50fbc782fcd1191da17a4c3986caff16f78
許景欣 <xjx0106@gmail.com>
2021-09-08 17:25:22 +0800

    feat: powerpoint background style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,68 @@
 <template>
   <div class="home">
+    <div
+      class="background"
+      :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }"
+    ></div>
     <div class="text">home page</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
-}
+  name: "Home",
+  data() {
+    return {
+      backgroundImageUrl: require("../assets/image/bg4.jpg"),
+    };
+  },
+  created() {
+    let bgCount = 1;
+    setInterval(() => {
+      this.setBackground(bgCount);
+      if (bgCount === 4) {
+        bgCount = 1;
+      } else {
+        bgCount++;
+      }
+    }, 15000);
+  },
+  computed: {},
+  methods: {
+    setBackground(bgCount) {
+      if(bgCount === 1) {
+        this.backgroundImageUrl = require("../assets/image/bg1.jpg");
+      }
+      if(bgCount === 2) {
+        this.backgroundImageUrl = require("../assets/image/bg2.jpg");
+      }
+      if(bgCount === 3) {
+        this.backgroundImageUrl = require("../assets/image/bg3.jpg");
+      }
+      if(bgCount === 4) {
+        this.backgroundImageUrl = require("../assets/image/bg4.jpg");
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .home {
-  height: 100%;
   width: 100%;
-  background-color: rgb(0, 3, 41);
+  height: 100%;
+  background-color: black;
+  .background {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+    background-size: cover;
+    transition: background-image 6s;
+    filter: blur(4px);
+  }
   .text {
     color: white;
     font-size: 40px;
--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -38,7 +38,7 @@ export default {
           this.dots = ". . .";
           break;
       }
-    },
+    }
   },
 };
 </script>
