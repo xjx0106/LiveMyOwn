d67b51512ad440c815a198b671d679c29da62150
許景欣 <xjx0106@gmail.com>
2021-09-09 14:50:14 +0800

    feat: mian area in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
     <div class="background">
-      <video type="video/mp4" loop class="video" muted @canplay="videoLoaded" ref="video">
-        <source src="@/assets/video/neon-bg2.mp4" id="v1" />
-        <source src="@/assets/video/neon-bg1.mp4" id="v2" />
+      <video
+        type="video/mp4"
+        loop
+        class="video"
+        muted
+        @canplay="videoLoaded"
+        ref="video"
+      >
+        <source src="@/assets/video/neon-bg.mp4" id="v1" />
       </video>
     </div>
-    <div class="text">home page</div>
+    <div class="mian-area">
+      <div class="mian-left">
+        <div class="mian-title">
+          標題標題標題
+        </div>
+      </div>
+      <div class="mian-right"></div>
+    </div>
   </div>
 </template>
 
@@ -15,20 +28,20 @@ import ee from "@/components/eventEmitter.js";
 export default {
   name: "Home",
   data() {
-    return {
-
-    };
+    return {};
   },
-  created() {
-
-  },
+  created() {},
   computed: {},
   methods: {
-    videoLoaded() {
+    async videoLoaded() {
       console.log("videoLoaded, emit home-loaded");
-      ee.emit("home-loaded");
-      this.$refs.video.play();
-    }
+      setTimeout(() => {
+        ee.emit("home-loaded");
+        this.$nextTick(() => {
+          this.$refs.video.play();
+        });
+      }, 3000);
+    },
   },
 };
 </script>
@@ -39,14 +52,9 @@ export default {
   height: 100%;
   background-color: black;
   .background {
-    position: absolute;
-    top: 0px;
-    left: 0px;
+    position: fixed;
     height: 100%;
     width: 100%;
-    // z-index: 1;
-    // background-size: cover;
-    // transition: background-image 6s;
     // filter: blur(4px);
     .video {
       height: 100%;
@@ -54,10 +62,40 @@ export default {
       object-fit: cover;
     }
   }
-  .text {
+  .mian-area {
+    // z-index: 1;
     color: white;
     font-size: 40px;
-    padding-top: 40px;
+    height: 100vh;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    .mian-left {
+      height: 90%;
+      width: 45%; 
+      background-color: #00000000;
+      display: flex;
+      align-items: center;
+      .mian-title {
+        text-align: center;
+        color: white;
+        width: 100%;
+        font-size: 54px;
+        // border: 1px solid red;
+        height: fit-content;
+      }
+    }
+    .mian-right {
+      height: 90%;
+      width: 45%;
+      background-color: #00000099;
+    }
+    > div {
+      width: 50px;
+      height: 50px;
+    }
   }
 }
 </style>
